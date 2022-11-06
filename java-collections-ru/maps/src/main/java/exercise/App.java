f097d084a034d90e2ae157a8cf3c1735722c3743
@hexlet/cli <support@hexlet.io>
2022-11-06 19:54:05 +0700

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,36 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+class App {
+    public static Map<String, Integer> getWordCount(String suggestion) {
+        Map<String, Integer> dictionary = new HashMap<>();
+        String[] words = suggestion.split(" ");
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                Integer count = dictionary.get(word);
+                if (count == null) {
+                    count = 0;
+                }
+                dictionary.put(word, ++count);
+            }
+        }
+        return dictionary;
+    }
+
+    public static String toString(Map<String, Integer> dictionary) {
+        if (dictionary.isEmpty()) {
+            return "{}";
+        }
+        var temp = new StringBuilder();
+        temp.append("{");
+        for (Map.Entry<String, Integer> count : dictionary.entrySet()) {
+            temp.append("\n   ").append(count.getKey()).append(":  ").append(count.getValue());
+        }
+        temp.append("\n}");
+        return String.valueOf(temp);
+    }
+}
+//END
