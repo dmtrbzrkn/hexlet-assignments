413c82e8690670cb58be5058cc602595778f2b8c
@hexlet/cli <support@hexlet.io>
2023-03-25 15:42:18 +0700

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+// BEGIN
+public class App {
+    public static void save(Path path, Car car) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File(path.toString()), car);
+    }
+
+    public static Car extract(Path path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(new File(path.toString()), Car.class);
+    }
+}
+// END
